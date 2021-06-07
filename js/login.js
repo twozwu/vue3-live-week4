bdc0231df54421ced9d1bfa6dea2a2d6d5fda5b7
onedog <honter_tribor@hotmail.com>
2021-06-08 01:47:40 +0800

    20210608 renew

--- js/login.js
+++ js/login.js
@@ -1,30 +1,66 @@
-const emailInput = document.querySelector("#email");
-const pwInput = document.querySelector("#password");
-const checkBtn = document.querySelector("#check");
-const getProductsBtn = document.querySelector("#getProducts");
-const addProductBtn = document.querySelector("#addProduct");
+// const emailInput = document.querySelector("#email");
+// const pwInput = document.querySelector("#password");
+// const checkBtn = document.querySelector("#check");
+// const getProductsBtn = document.querySelector("#getProducts");
+// const addProductBtn = document.querySelector("#addProduct");
 
-const url = "https://vue3-course-api.hexschool.io";
+// const url = "https://vue3-course-api.hexschool.io";
 
-function login() {
-  const username = emailInput.value.trim();
-  const password = pwInput.value;
-  const user = { username, password };
-  // console.log(user);
-  axios
-    .post(`${url}/admin/signin`, user) //請求的方法
-    .then((res) => {
-      if (res.data.success == true) {
-        const { token, expired } = res.data;
-        console.log(token, expired);
-        document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
-        window.location = "products.html";
-      } else {
-        alert("帳號或密碼錯誤");
-      }
-    })
-    .catch((error) => {
-      console.dir(error);
-    });
-  return false;
-}
+// function login() {
+//   const username = emailInput.value.trim();
+//   const password = pwInput.value;
+//   const user = { username, password };
+//   // console.log(user);
+//   axios
+//     .post(`${url}/admin/signin`, user) //請求的方法
+//     .then((res) => {
+//       if (res.data.success == true) {
+//         const { token, expired } = res.data;
+//         console.log(token, expired);
+//         document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+//         window.location = "products.html";
+//       } else {
+//         alert("帳號或密碼錯誤");
+//       }
+//     })
+//     .catch((error) => {
+//       console.dir(error);
+//     });
+//   return false;
+// }
+
+const app = Vue.createApp({
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      url: "https://vue3-course-api.hexschool.io",
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`${this.url}/admin/signin`, this.user) //請求的方法
+        .then((res) => {
+          if (res.data.success == true) {
+            const { token, expired } = res.data;
+            console.log(token, expired);
+            document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+            window.location = "products.html";
+          } else {
+            alert("帳號或密碼錯誤");
+          }
+        })
+        .catch((error) => {
+          console.dir(error);
+        });
+    },
+  },
+  mounted() {
+    console.log("test");
+  },
+});
+
+app.mount("#app");
